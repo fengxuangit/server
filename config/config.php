12fff28811ced85fd5d24b1cb653309d1292d81e
fengxuangit <978348306@qq.com>
2017-12-14 12:05:37 +0800

    first commit

---
+++ config/config.php
@@ -0,0 +1,21 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocps5jc6038i',
+  'passwordsalt' => 'okrdXoG4h3XJhI5/kgcoKItgpESDOB',
+  'secret' => 'a8ViYpdpQVIqMcutErMiI/1zfTt2lvoCyIrZNPWgSggMasrp',
+  'trusted_domains' => 
+  array (
+    0 => 'cloud.evalshell.com',
+  ),
+  'datadirectory' => '/data/www/cloud.evalshell.com/data',
+  'overwrite.cli.url' => 'http://cloud.evalshell.com',
+  'dbtype' => 'mysql',
+  'version' => '12.0.4.3',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'oc_admin',
+  'dbpassword' => 'W7JRY73i0yfbnBzlSrE+xJdGMg1iF4',
+  'installed' => true,
+);
